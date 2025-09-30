e8313628327a00594d17e85237527c84bf7618ca
Prathyush2007 <prathyush.nimmagadda.btech2024@sitpune.edu.in>
2025-09-30 22:22:28 +0530

    Create Linear Search.cpp

---
+++ Linear Search.cpp
@@ -0,0 +1,30 @@
+//Name : Prathyush
+//PRN : 24070123064
+//Batch : ENTC A3
+#include <iostream>
+using namespace std;
+
+int linearSearch(int arr[], int size, int target) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            return i;  
+        }
+    }
+    return -1;  
+}
+
+int main() {
+    int numbers[] = {10, 25, 30, 45, 50};
+    int target = 30;
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+
+    int index = linearSearch(numbers, size, target);
+
+    if (index != -1) {
+        cout << "Target " << target << " found at index " << index << "." << endl;
+    } else {
+        cout << "Target " << target << " not found in the array." << endl;
+    }
+
+    return 0;
+}
